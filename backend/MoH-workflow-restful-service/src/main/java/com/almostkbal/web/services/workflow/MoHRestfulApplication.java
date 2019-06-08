d976143a048c796beb63a4721fcd9f5676071f10
msrashed2018 <msrashed100@gmail.com>
2019-06-08 17:03:15 +0200

    init and clean up storage service

--- backend/MoH-workflow-restful-service/src/main/java/com/almostkbal/web/services/workflow/MoHRestfulApplication.java
+++ backend/MoH-workflow-restful-service/src/main/java/com/almostkbal/web/services/workflow/MoHRestfulApplication.java
@@ -1,11 +1,26 @@
 package com.almostkbal.web.services.workflow;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.almostkbal.web.services.workflow.services.StorageService;
+
 @SpringBootApplication
-public class MoHRestfulApplication {
+public class MoHRestfulApplication implements ApplicationRunner{
+	
+	@Autowired
+	private StorageService storageService;
 	public static void main(String[] args) {
 		SpringApplication.run(MoHRestfulApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		storageService.deleteAll();
+		storageService.init();
+		
 	}
 }
